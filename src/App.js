5a77ce0a5d2df6d430b9c7a8187718503ba91c89
curiosus <curiosus@gmail.com>
2019-12-17 08:02:08 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+import cat1 from './cats/cat.173.jpg';
+import cat2 from './cats/cat.8474.jpg';
+import cat3 from './cats/cat.6662.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<Carousel width={600}>
+			<div>
+				<img src={cat1} alt='cat'  />
+				<p className='legend'>Cat 1</p>
+		  </div>
+			<div>
+				<img src={cat2} alt='cat' />
+				<p className='legend'>Cat 2</p>
+		  </div>
+			<div>
+				<img src={cat3} alt='cat' />
+				<p className='legend'>Cat 3</p>
+		  </div>
+
+
+		</Carousel>
+	);
 }
 
 export default App;
